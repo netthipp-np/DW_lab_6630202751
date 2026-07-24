90cf409c7dc84129ba124f4080beaa4f4b4e6dc6
netthipp-np <netthip.p@kkumail.com>
2026-07-24 05:09:21 +0000

    dim

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.markdown('<div class="main-title">📊 Northwind DW Explorer</div>', unsafe_allow_html=True)
+st.markdown('<div class="main-title">📊 Northwind DW Explorer(663020275-1 Netthip Phrommasit)</div>', unsafe_allow_html=True)
 st.markdown('<div class="subtitle">Inspect and preview raw datasets, staging tables, and dimension views in dev.duckdb</div>', unsafe_allow_html=True)
 
 # Fetch all tables
